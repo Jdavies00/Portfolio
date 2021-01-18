63a92357c08b0b96f9bb16850a5af29b7ffe1212
James Davies <j.davies2099@gmail.com>
2021-01-18 15:40:30 -0500

    fixed typo/restructured for respondsivness/ need to add more content

--- src/component/cards.js
+++ src/component/cards.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
-import CardDeck from 'react-bootstrap/CardDeck';
 import { Container, Row, Col } from 'reactstrap';
 import Game from "./TicTacToe/Game";
 import Clock from './clock';
 import Calculator from "./calculator";
 import '/Users/jamesdavies/Sites/Portfolio /portfolio/src/css/card.css';
 import TodoList from "./TodoList/TodoList";
-// import SlidingPuzzle from "./slidingPuzzle/SPapp"
+import SlidingPuzzle from "./slidingPuzzle/SPapp"
 
 export default function Cards() {
     const standAloneStyle = {
@@ -21,76 +20,98 @@ export default function Cards() {
     return (
         <>
             <Container>
-                <CardDeck>
-                    <Card>
-                        <Card.Body className="text-center">
-                            <Card.Title>Clock | Timer</Card.Title>
-                            <Clock id="clock" />
-                        </Card.Body>
-                        <Card.Footer>
-                            <small className="text-muted"> </small>
-                        </Card.Footer>
-                    </Card>
-                </CardDeck>
+
+                <Row>
+                    <Col sm="12" md={{ size: 6, offset: 3 }}>
+                        <Card>
+                            <Card.Body className="text-center">
+                                <Card.Title>Clock | Timer</Card.Title>
+                                <Clock id="clock" />
+                            </Card.Body>
+                            <Card.Footer>
+                                <small className="text-muted"> </small>
+                            </Card.Footer>
+                        </Card>
+                    </Col>
+                </Row>
+
                 <br />
-                <CardDeck>
-                    <Card>
-                        <Card.Title>Calculator</Card.Title>
-                        <Calculator />
-                        <Card.Footer >
-                            <small className="text-muted"> </small>
-                        </Card.Footer>
-                    </Card>
-                    <Card>
-                        <Card.Body>
-                            <Card.Title>To-Do List</Card.Title>
-                            <TodoList />
-                        </Card.Body>
-                        <Card.Footer>
-                            <small className="text-muted"> </small>
-                        </Card.Footer>
-                    </Card>
-                    <Card style={ticTacToeStyle}>
-                        <Card.Title className='text-center'>Tic-Tac-Toe</Card.Title>
-                        <Game />
-                    </Card>
-                </CardDeck>
+                <Row>
+                    <Col xs="auto" md="6">
+                        <Card>
+                            <Card.Title className="text-center">Calculator</Card.Title>
+                            <Calculator />
+                            <Card.Footer >
+                                <small className="text-muted"> </small>
+                            </Card.Footer>
+                        </Card>
+                    </Col>
+
+                    <Col xs="auto" md="6">
+                        <Card style={ticTacToeStyle}>
+                            <Card.Title className='text-center'>Tic-Tac-Toe</Card.Title>
+                            <Game />
+                            <Card.Footer>
+                                <small className="text-muted"> </small>
+                            </Card.Footer>
+                        </Card>
+                    </Col>
+                </Row>
+
                 <br />
-                <CardDeck>
-                    <Card>
-                        <Card.Body>
-                            <Card.Title>Sliding Puzzle</Card.Title>
-                            {/* <SlidingPuzzle /> */}
-                        </Card.Body>
-                        <Card.Footer>
-                            <small className="text-muted"> </small>
-                        </Card.Footer>
-                    </Card>
-                    <Card>
-                        <Card.Body style={standAloneStyle} className="text-center">
-                            <Card.Title >StandAlones</Card.Title>
-                            <i className="fas fa-blog"> Blog</i>
-                            <br />
-                            <a href="https://github.com/Jdavies00">
-                                <i className="fab fa-github"> Github</i></a>
-                            <a href="https://jdavies00.github.io/weather-API/">
-                                <i class="fas fa-cloud">Weather API</i></a>
-                            <br />
-                            <a href="https://jdavies00.github.io/MindReader/"><i class="fab fa-readme">Mind Reader App </i></a>
-                        </Card.Body>
-                        <Card.Footer>
-                            <small className="text-muted"> </small>
-                        </Card.Footer>
-                    </Card>
-                </CardDeck>
+
+                <Row>
+                    <Col xs="auto" md="6">
+                        <Card>
+                            <Card.Body className="text-center">
+                                <Card.Title>To-Do List</Card.Title>
+                                <TodoList />
+                            </Card.Body>
+                            <Card.Footer>
+                                <small className="text-muted"> </small>
+                            </Card.Footer>
+                        </Card>
+                    </Col>
+
+                    <br />
+
+                    <Col xs="auto" md="6">
+                        <Card className="text-center">
+                            <Card.Body>
+                                <Card.Title>Sliding Puzzle</Card.Title>
+                                <SlidingPuzzle />
+                            </Card.Body>
+                            <Card.Footer>
+                                <small className="text-muted"> </small>
+                            </Card.Footer>
+                        </Card>
+                    </Col>
+
+                    <Col xs="auto" md="6">
+                    <br />
+                        <Card>
+                            <Card.Body style={standAloneStyle} className="text-center">
+                                <Card.Title >StandAlones</Card.Title>
+                                <a href="https://jdaviesblog-a14a2.firebaseapp.com/">
+                                    <i className="fas fa-blog"> Blog</i></a>
+                                <br />
+                                <a href="https://github.com/Jdavies00">
+                                    <i className="fab fa-github"> Github</i></a>
+                                <a href="https://jdavies00.github.io/weather-API/">
+                                    <i class="fas fa-cloud">Weather API</i></a>
+                                <br />
+                                <a href="https://jdavies00.github.io/MindReader/"><i class="fab fa-readme">Mind Reader App </i></a>
+                            </Card.Body>
+                            <Card.Footer>
+                                <small className="text-muted"> </small>
+                            </Card.Footer>
+                        </Card>
+                    </Col>
+
+                </Row>
             </Container>
-            <Row>
-                <Col>
-                    <footer id="footer">
-                        <span className="text-muted">sometext</span>
-                    </footer>
-                </Col>
-            </Row>
         </>
+
+
     )
 }
